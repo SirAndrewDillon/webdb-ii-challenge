264a19cb9e03dde29d41d8644dc686c13eee120d
andrew dillon <majik895@gmail.com>
2019-08-14 10:56:10 -0400

    Added all CRUD

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -5,15 +5,69 @@ const config = require("../knexfile.js");
 
 const db = knex(config.development);
 
-const router = require('express').Router();
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const cars = await db('cars');
+        res.json(cars);
+    } catch (err) {
+        res.status(500).json({ message: 'Failed to retrieve cars' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const car = await db('cars').where({ id });
+
+        res.json(car);
+    } catch (err) {
+        res.status(500).json({ message: 'Failed to retrieve car' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const carData = req.body;
+        const [id] = await db('cars').insert(carData);
+        const newCarEntry = await db('cars').where({ id });
+
+        res.status(201).json(newCarEntry);
+    } catch (err) {
+        console.log('POST error', err);
+        res.status(500).json({ message: "Failed to store data" });
+    }
+});
+
+router.put('/:id', (req, res) => {
+    db('cars')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: `${count} record(s) updated` });
+            } else {
+                res.status(404).json({ message: "record not found" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
 
 
-
-
-
-
-
-
+router.delete('/:id', (req, res) => {
+    db('cars')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            res.status(200).json({ message: `${count} record(s) deleted` });
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
 
 
 
